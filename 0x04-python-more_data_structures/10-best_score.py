8a87fe6e3a44cceeb0e125d36061127b001f9372
Wasiu Bakare <wasiubakare@gmail.com>
2022-06-08 17:23:45 +0100

    big key

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     """Returns a key with the biggest integer value."""
-    if not isinstance(a_dictionary, dict) or a_dictionary is None:
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
     return sorted(a_dictionary)[-1]

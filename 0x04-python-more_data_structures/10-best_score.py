b904680824c67fbaf023ab642d830c18cac462a1
Wasiu Bakare <wasiubakare@gmail.com>
2022-06-08 17:30:51 +0100

    big key refactored

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,4 +3,11 @@ def best_score(a_dictionary):
     """Returns a key with the biggest integer value."""
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
-    return sorted(a_dictionary)[-1]
+    first_key = list(a_dictionary.keys())[0]
+    max_value = a_dictionary[first_key]
+
+    for key, value in a_dictionary.items():
+        if value > max_value:
+            max_value = value
+            first_key = key
+    return max_value

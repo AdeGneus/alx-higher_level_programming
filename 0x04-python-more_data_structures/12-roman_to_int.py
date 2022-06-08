9e3b4fe64edcecff90024f1aebe14b8c2e1f8765
Wasiu Bakare <wasiubakare@gmail.com>
2022-06-08 22:15:28 +0100

    fixed code failure on some edge cases

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     len_roman_num = len(roman_string)
     sum_num = 0
-    roman_num_dict = {
+    rom_num = {
         "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000,
     }
     if (not isinstance(roman_string, str) or
@@ -10,11 +10,13 @@ def roman_to_int(roman_string):
         return 0
     else:
         for x in range(len_roman_num):
+            if rom_num.get(roman_string[x], 00) == 0:
+                return 0
             if x is (len_roman_num - 1):
-                sum_num += roman_num_dict[roman_string[x]]
+                sum_num += rom_num[roman_string[x]]
             else:
-                if roman_num_dict[roman_string[x]] >= roman_num_dict[roman_string[x + 1]]:
-                    sum_num += roman_num_dict[roman_string[x]]
+                if rom_num[roman_string[x]] >= rom_num[roman_string[x + 1]]:
+                    sum_num += rom_num[roman_string[x]]
                 else:
-                    sum_num -= roman_num_dict[roman_string[x]]
+                    sum_num -= rom_num[roman_string[x]]
     return sum_num

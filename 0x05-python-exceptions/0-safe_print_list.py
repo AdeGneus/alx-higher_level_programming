057eaecc45f6b809677e2098afbd8e256ff3cde5
Wasiu Bakare <wasiubakare@gmail.com>
2022-06-20 13:28:07 +0100

    fixed edge cases

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,10 +4,10 @@ def safe_print_list(my_list=[], x=0):
     """Prints x elements of a list"""
 
     length = 0
-    for i in my_list:
+    for i in range(x):
         try:
+            print("{}".format(my_list[i]), end="")
             length += 1
-            print("{}".format(i), end="")
         except IndexError:
             break
     print("")

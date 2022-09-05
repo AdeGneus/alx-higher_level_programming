a612fe803bf19f40ce7db0efb101ce4da81374ea
Wasiu Bakare <wasiubakare@gmail.com>
2022-09-05 19:14:57 +0100

    Fix semistandard error

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,7 +2,7 @@
 const firstNum = Math.trunc(process.argv[2]);
 const secondNum = Math.trunc(process.argv[3]);
 
-function add(a, b) {
+function add (a, b) {
   console.log(a + b);
 }
 add(firstNum, secondNum);

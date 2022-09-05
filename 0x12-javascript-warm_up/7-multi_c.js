295a9643196f06d238ff1d810e12a61332db54f5
Wasiu Bakare <wasiubakare@gmail.com>
2022-09-05 18:44:50 +0100

    Refactor 7-multi_c

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,6 +2,4 @@
 const firstArg = Math.trunc(process.argv[2]);
 
 if (isNaN(firstArg)) console.log('Missing number of occurrences');
-else {
-  for (let i = 0; i < firstArg; i++) console.log('C is fun');
-}
+for (let i = 0; i < firstArg; i++) console.log('C is fun');

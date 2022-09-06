86ddc3329580ac0f67f46fb798102569c31bbf7c
Wasiu Bakare <wasiubakare@gmail.com>
2022-09-06 19:08:50 +0100

    Fix semistandard issue

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const Square = require("./5-square");
+const Square = require('./5-square');
 
 class Square1 extends Square {
-  charPrint(c) {
+  charPrint (c) {
     if (!c) this.print();
     else {
       for (let i = 0; i < this.width; i++) console.log(c.repeat(this.height));
